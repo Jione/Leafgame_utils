0e1427950be9d2876f9d3b559315061454157a7b
Jione <lawfull@naver.com>
2025-06-28 20:02:55 +0900

    lzss update

--- Kizuato/packutil/lzss.h
+++ Kizuato/packutil/lzss.h
@@ -6,6 +6,14 @@
 #include <ostream>
 #include <istream>
 
+// LZSS 압축 / 압축 해제 전역 설정
+const char initFillChar = 0x20;
+const bool isPrefillCompress = false;
+const bool isXorData = false;
+const bool isRotateFlag = false;
+const bool isShiftFlagData = false;
+const char initShiftChar[5] = "DACB";
+
 // 입력 파일을 LZSS 방식으로 압축하여 지정된 출력 스트림에 기록
 bool lzss_compress_file_to_stream(const std::wstring& inputFilePath, std::ostream& outputStream);
 
